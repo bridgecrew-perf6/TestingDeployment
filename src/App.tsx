cdbee3ec87fa97a5a2a7ac9f5f0920000c3940fd
Nahomi <nahyomi2000@gmail.com>
2022-05-10 11:36:46 -0500

    test for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import {Children, useEffect, useState} from 'react';
 import './App.css';
+import Form from './Components/Form';
+import List from './Components/List';
+import { Sub } from './Components/types';
 
+
+
+const initial_State = [
+  {
+    nick:"Nahomi",
+    subMonths: 21,
+    avatar: "https://i.pravatar.cc/150?u=nao",
+    description: "loremdjpiwshdldl sqihfpjwqfjpqhf"
+  },
+  {
+    nick:"Tana",
+    subMonths: 34,
+    avatar: "https://i.pravatar.cc/150?u=tana",
+  }
+]
 function App() {
+  const [subs, setSubs] = useState<Array<Sub>>([])
+  const [newSubsNumber,setNewSubsNumber] = useState<any>(0)
+  
+  useEffect(()=>{
+    setSubs(initial_State)
+  }, [])
+
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs,newSub])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>midu subs</h1>
+      <List subs= {subs}/>
+      <Form  onNewSub={handleNewSub}/>
     </div>
   );
 }
